d17d9dc8cc60e55987d55a72837aa323018ac5f7
krganeshrajhan <krganeshrajhan@gmail.com>
2018-03-06 20:22:32 +0800

    Dbpoller

---
+++ spring-example/dbpoller/dbpoller_mysql/src/test/java/org/javacodegeeks/springintegration/polling/dbpoller/DbpollerApplicationTests.java
@@ -0,0 +1,17 @@
+package org.javacodegeeks.springintegration.polling.dbpoller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DbpollerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
