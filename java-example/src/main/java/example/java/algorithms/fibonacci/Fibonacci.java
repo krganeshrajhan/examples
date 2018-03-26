58c00c3fdec244c35279370c3fe4bd29523e7169
Ganeshrajhan <krganeshrajhan@gmail.com>
2018-03-26 14:45:52 +0800

    Fibonacci

---
+++ java-example/src/main/java/example/java/algorithms/fibonacci/Fibonacci.java
@@ -0,0 +1,26 @@
+package example.java.algorithms.fibonacci;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static int fibonacci(int n) {
+		// Complete the function.
+		if(n==0) {
+			return 0;
+		} else if(n==1) {
+			return 1;
+		} else {
+			return fibonacci(n-1) + fibonacci(n-2);
+		}
+
+	}
+
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		scanner.close();
+		System.out.println(fibonacci(n));
+	}
+}
